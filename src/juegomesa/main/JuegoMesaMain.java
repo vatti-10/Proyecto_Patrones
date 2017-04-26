ee2f98a8c3ebc89bf86ae6784b335dd02204bc5f
carlo <carlosalvarado310.ca@gmail.com>
2017-04-25 20:56:07 -0600

    HolaMundo 2

--- src/juegomesa/main/JuegoMesaMain.java
+++ src/juegomesa/main/JuegoMesaMain.java
@@ -16,6 +16,7 @@ public class JuegoMesaMain {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo2!");
     }
     
 }
