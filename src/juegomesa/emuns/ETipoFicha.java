4aaa2b4f6bc268be8d154cf65c82890ed0b95a56
Jorge Diaz <jorgeda@gmail.com>
2017-05-01 22:39:38 -0600

    Creacion del Enum ETipoFicha, con todos los posibles tipos de fichas que posee la aplicacion.

---
+++ src/juegomesa/emuns/ETipoFicha.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegomesa.emuns;
+
+/**
+ *
+ * @author jmda9
+ */
+public enum ETipoFicha {
+    GO,
+    DAMAS_NORMAL,
+    DAMAS_CORONA,
+    PEON,
+    CABALLO,
+    TORRE,
+    ARFIL,
+    REINA,
+    REY    
+}
