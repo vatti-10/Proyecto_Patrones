ed4fc4ebc43f4eb4c1d1346f8d14930201dc35c9
hmari_001 <hmaria26@gmail.com>
2017-05-05 17:33:31 -0600

    Se crea clase fabrica de tipo de juego

---
+++ src/juegomesa/utils/FabricaTipoJuego.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegomesa.utils;
+
+import juegomesa.emuns.ETipoJuego;
+import juegomesa.utils.tipojuego.ITipoJuego;
+
+/**
+ *
+ * @author hmari_001
+ */
+public class FabricaTipoJuego {
+    public static JuegoMesa FabricarTipoJuego(ETipoJuego ){
+        
+    }
+}
