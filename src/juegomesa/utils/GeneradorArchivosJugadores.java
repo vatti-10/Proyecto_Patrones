21b0e119c8a14a3a9fab88f2c0a7f651c98b4df1
Carlos <carlosalvarado310.ca@gmail.com>
2017-05-05 22:50:00 -0600

    Se crea la clase generadora de archivos de jugadores

---
+++ src/juegomesa/utils/GeneradorArchivosJugadores.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegomesa.utils;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Properties;
+/**
+ *
+ * @author carlos
+ */
+public class GeneradorArchivosJugadores {
+    public static void generarArchivoJugador(String pUsername,String pEmail, String pPassword){
+       Properties property=new Properties();
+       FileInputStream input=null;
+       try {
+            input = new FileInputStream("src\\juegomesa\\properties\\rutas.properties");
+            String filename=pUsername+".txt";
+            property.load(input);
+            String ruta=property.getProperty("ruta_archivos");
+            File archivo = new File(ruta+filename);
+            BufferedWriter bw = null;
+            if(!archivo.exists()) {
+                bw = new BufferedWriter(new FileWriter(archivo));
+                bw.write("{username: "+pUsername+", email: "+pEmail+", password: "+pPassword+"}");
+            }
+            bw.close();
+        }catch (IOException ex) {
+        ex.printStackTrace();
+        } finally {
+            if (input != null) {
+             try {
+                input.close();
+             } catch (IOException e) {
+                e.printStackTrace();
+                }
+            }
+        }
+    }
+}
