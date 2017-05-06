610f1392ad44d50a7949a154cfc3d277c89356ab
Jorge Diaz <jorgeda@gmail.com>
2017-05-05 20:42:57 -0600

    Se crea la clase FichaAjedrezCaballo. Aun pendiente implementacion de los métodos de verificación.

---
+++ src/juegomesa/fichas/FichaAjedrezCaballo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegomesa.fichas;
+
+import juegomesa.emuns.EColorJugador;
+import juegomesa.emuns.ETipoFicha;
+
+/**
+ *
+ * @author jmda9
+ */
+public class FichaAjedrezCaballo extends Ficha{
+
+    public FichaAjedrezCaballo(int pCoordX, int pCoordY, EColorJugador pColor) {
+        super(pCoordX, pCoordY, ETipoFicha.CABALLO, pColor);
+    }
+
+    @Override
+    public boolean verificarMovimientoValido(int[] pCoordenadas) {
+        
+    }
+    
+}
