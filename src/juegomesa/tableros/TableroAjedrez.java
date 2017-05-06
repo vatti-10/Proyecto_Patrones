50dfa8942ac31c89e1bf576c69183929b4ad36ae
Carlos <carlosalvarado310.ca@gmail.com>
2017-05-06 11:56:23 -0600

    Se agrego funcionalidad a la clase TableroAjedrez

--- src/juegomesa/tableros/TableroAjedrez.java
+++ src/juegomesa/tableros/TableroAjedrez.java
@@ -7,7 +7,9 @@ package juegomesa.tableros;
 
 import juegomesa.emuns.EColorJugador;
 import juegomesa.emuns.ETipoFicha;
+import juegomesa.emuns.ETipoJuego;
 import juegomesa.fichas.Ficha;
+import juegomesa.utils.FabricaConvertidoresInputCoordenada;
 
 /**
  *
@@ -15,6 +17,10 @@ import juegomesa.fichas.Ficha;
  */
 public class TableroAjedrez extends Tablero{
 
+    public TableroAjedrez() {
+        super();
+    }
+    
     @Override
     public void construirCasillasTablero() {
         casillasTablero=new Casilla[8][8];
@@ -24,19 +30,29 @@ public class TableroAjedrez extends Tablero{
 
     @Override
     public int[] getCoordenadasNumericas(String pCoordenadas) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return FabricaConvertidoresInputCoordenada.fabricarConvertidor(ETipoJuego.AJEDREZ).convertir(pCoordenadas);
     }
 
     @Override
-    public void actualizarTablero(String pCoordenadas) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean actualizarTablero(String pCoordenadas,EColorJugador pColorJugador) {
+        int [] coordenadas=getCoordenadasNumericas(pCoordenadas);
+        if(verificarCoordenadas(pCoordenadas)&&estaCasillaVacia(coordenadas[0], coordenadas[1])){
+            return realizarJugada(pCoordenadas,pColorJugador);
+        }
+        return false;
     }
 
     @Override
     public boolean verificarCoordenadas(String pCoordenadas) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return validarCoordenadas(getCoordenadasNumericas(pCoordenadas));
     }
-
+    
+    @Override
+    public boolean validarCoordenadas(int[] pCoordenadas){
+        return pCoordenadas[0]<getCasillasTablero()[0].length
+                && pCoordenadas[1]<getCasillasTablero().length;
+    }
+    
     private void ubicarFichas(){
         ubicarFichasBlancas();
         ubicarFichasNegras();
@@ -69,5 +85,90 @@ public class TableroAjedrez extends Tablero{
         for (int i = 0; i < getCasillasTablero().length; i++) {
             getCasillasTablero()[pFila][i].setFicha(new Ficha(0, 0, ETipoFicha.PEON, EColorJugador.BLANCO));
         }
+    }
+
+    private boolean realizarJugada(String pCoordenadas,EColorJugador pColorJugador) {
+        if(pCoordenadas.length()==2 || Character.isLowerCase(pCoordenadas.charAt(0))){
+            return realizarJugadaConPeon(pCoordenadas,pColorJugador);
+        }else if(pCoordenadas.length()>=3 &&pCoordenadas.charAt(0)=='B'){
+            return verificarJugada(pCoordenadas,pColorJugador,ETipoFicha.ARFIL);
+        }
+        else if(pCoordenadas.length()>=3 &&pCoordenadas.charAt(0)=='R'){
+            return verificarJugada(pCoordenadas,pColorJugador,ETipoFicha.TORRE);
+        }
+        else if(pCoordenadas.length()>=3 &&pCoordenadas.charAt(0)=='N'){
+            return verificarJugada(pCoordenadas,pColorJugador,ETipoFicha.CABALLO);
+        }
+        else if(pCoordenadas.length()>=3 &&pCoordenadas.charAt(0)=='Q'){
+            return verificarJugada(pCoordenadas,pColorJugador,ETipoFicha.REINA);
+        }
+        else if(pCoordenadas.length()>=3 &&pCoordenadas.charAt(0)=='K'){
+            return verificarJugada(pCoordenadas,pColorJugador,ETipoFicha.REY);
+        }
+        return false;
+    }
+    private boolean verificarJugada(String pCoordenadas, EColorJugador pColorJugador, ETipoFicha pTipoFicha) {
+        if(pCoordenadas.charAt(1)=='x'){
+            return comerFicha(pCoordenadas,pColorJugador,pTipoFicha);
+        }else if(pCoordenadas.length()==3){
+            return moverFicha(pCoordenadas,pColorJugador,pTipoFicha);
+        }
+        return false;
+    }
+    
+    private boolean moverFicha(String pCoordenadas, EColorJugador pColorJugador, ETipoFicha pTipoFicha) {
+        int [] coordenadas=getCoordenadasNumericas(pCoordenadas);
+        if(estaCasillaVacia(coordenadas[0],coordenadas[1])){
+            return buscarFicha(coordenadas, pColorJugador, pTipoFicha);
+        }
+        return false;
+    }
+    
+    private boolean realizarJugadaConPeon(String pCoordenadas,EColorJugador pColorJugador){
+        if(pCoordenadas.length()==2){
+            return moverPeon(pCoordenadas,pColorJugador);
+        }else if(pCoordenadas.charAt(1)=='x'){
+            return comerFicha(pCoordenadas,pColorJugador,ETipoFicha.PEON);
+        }
+        return false;
+    }
+    private boolean moverPeon(String pCoordenadas, EColorJugador pColorJugador) {
+        int [] coordenadas=getCoordenadasNumericas(pCoordenadas);
+        if(pColorJugador==EColorJugador.BLANCO && !estaCasillaVacia(coordenadas[0],coordenadas[1]-1) &&
+                getCasillasTablero()[coordenadas[1]-1][coordenadas[0]].getFicha().moverFicha(coordenadas)){
+            
+                getCasillasTablero()[coordenadas[1]][coordenadas[0]].setFicha(getFicha(coordenadas[0],coordenadas[1]-1));
+                getCasillasTablero()[coordenadas[0]][coordenadas[1]-1].setFicha(null);
+                return true;
+                
+        }else if(!estaCasillaVacia(coordenadas[0],coordenadas[1]+1)&& getCasillasTablero()[coordenadas[1]+1][coordenadas[0]].getFicha().moverFicha(coordenadas)){
+            
+                getCasillasTablero()[coordenadas[1]][coordenadas[0]].setFicha(getFicha(coordenadas[0],coordenadas[1]+1));
+                getCasillasTablero()[coordenadas[0]][coordenadas[1]+1].setFicha(null);
+                return true;
+        }
+        return false;
+    }
+
+    private boolean comerFicha(String pCoordenadas, EColorJugador pColorJugador,ETipoFicha pTipoFicha) {
+        String[] coordenadasString = pCoordenadas.split("x");
+        int [] coordenadas=getCoordenadasNumericas(coordenadasString[1]);
+        if(!estaCasillaVacia(coordenadas[0],coordenadas[1])&& getFicha(coordenadas[0],coordenadas[1]).getColor()!=pColorJugador){
+            return buscarFicha(coordenadas,pColorJugador,pTipoFicha);
+        }else
+            return false;
+    }
+
+    private boolean buscarFicha(int[] coordenadas, EColorJugador pColorJugador,ETipoFicha pTipoFicha) {
+        for (int i = 0; i < getCasillasTablero().length; i++) {
+            for (int j = 0; j < getCasillasTablero()[i].length; j++) {
+                if (!estaCasillaVacia(j, i) && getFicha(j, i).getColor()==pColorJugador &&getFicha(j, i).getTipoFicha()==pTipoFicha && getFicha(j, i).moverFicha(coordenadas)) {
+                    getCasillasTablero()[coordenadas[1]][coordenadas[0]].setFicha(getFicha(j, i));
+                    getCasillasTablero()[i][j].setFicha(null);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
