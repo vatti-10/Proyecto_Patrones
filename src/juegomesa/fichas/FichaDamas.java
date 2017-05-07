35a7c487063456515af295aa42bfda15bb9f64ce
Jorge Diaz <jorgeda@gmail.com>
2017-05-07 10:52:40 -0600

    Se crea la clase FichaDamas. Implementa los estados creados según el tipo de ficha que sea: normal o corona, y la implentacion de cada metodo va a depender del estado actual.

---
+++ src/juegomesa/fichas/FichaDamas.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegomesa.fichas;
+
+import juegomesa.emuns.EColorJugador;
+import juegomesa.emuns.ETipoFicha;
+import juegomesa.fichas.estadosfichas.EstadoFichaDamasCorona;
+import juegomesa.fichas.estadosfichas.EstadoFichaDamasNormal;
+import juegomesa.fichas.estadosfichas.IEstadoFichaDamas;
+
+/**
+ *
+ * @author jmda9
+ */
+public class FichaDamas extends Ficha{
+    
+    private IEstadoFichaDamas estadoNormal;
+    private IEstadoFichaDamas estadoCorona;
+    private IEstadoFichaDamas estado;
+
+    public FichaDamas(int pCoordX, int pCoordY, EColorJugador pColor) {
+        super(pCoordX, pCoordY, ETipoFicha.DAMAS_NORMAL, pColor);
+        setEstadoNormal(new EstadoFichaDamasNormal(this));
+        setEstadoCorona(new EstadoFichaDamasCorona(this));
+        setEstado(getEstadoNormal());
+    }
+
+    @Override
+    protected boolean verificarMovimientoValido(int[] pCoordenadas) {
+        return getEstado().verificarMovimientoValido(pCoordenadas);
+    }
+
+    @Override
+    protected void actualizarCoordenadas(int[] pCoordenadas) {
+        getEstado().actualizarCoordenadas(pCoordenadas);
+    }
+
+    /**
+     * @return the estadoNormal
+     */
+    public IEstadoFichaDamas getEstadoNormal() {
+        return estadoNormal;
+    }
+
+    /**
+     * @param estadoNormal the estadoNormal to set
+     */
+    public void setEstadoNormal(IEstadoFichaDamas estadoNormal) {
+        this.estadoNormal = estadoNormal;
+    }
+
+    /**
+     * @return the estadoCorona
+     */
+    public IEstadoFichaDamas getEstadoCorona() {
+        return estadoCorona;
+    }
+
+    /**
+     * @param estadoCorona the estadoCorona to set
+     */
+    public void setEstadoCorona(IEstadoFichaDamas estadoCorona) {
+        this.estadoCorona = estadoCorona;
+    }
+
+    /**
+     * @return the estado
+     */
+    public IEstadoFichaDamas getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(IEstadoFichaDamas estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
