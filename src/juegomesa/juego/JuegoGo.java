fc35b94ab9ac20be6b1408d01842ce403d769b2b
hmari_001 <hmaria26@gmail.com>
2017-05-06 18:31:35 -0600

    Se crea la clase juego go

---
+++ src/juegomesa/juego/JuegoGo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegomesa.juego;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import juegomesa.emuns.ETipoJuego;
+
+/**
+ *
+ * @author hmari_001
+ */
+public class JuegoGo extends JuegoMesa{
+    
+    public JuegoGo(Jugador pjugador1, Jugador pjugador2, ETipoJuego ptipoJuego){
+       super(pjugador1, pjugador2, ptipoJuego);
+    }
+    
+    public void almacenarDatosGeneralesJuego(){
+        Calendar fecha = new GregorianCalendar();
+        int anno = fecha.get(Calendar.YEAR);
+        int mes = fecha.get(Calendar.MONTH);
+        int dia = fecha.get(Calendar.DATE);
+        String date = "[DT" + anno + "-" + (mes + 1) + "-" + dia + "]";
+        String primerJugador = "PB[" + jugador1.getUsername() + "]";
+        String segundoJugador = "PW[" + jugador2.getUsername() + "]";
+        registrosJugadas.append(primerJugador);
+        registrosJugadas.append("\n");
+        registrosJugadas.append(segundoJugador);
+        registrosJugadas.append("\n");
+        registrosJugadas.append(date);
+        registrosJugadas.append("\n");
+    }
+    
+     @Override
+    public void almacenarJugada(String pjugada) {
+        registrosJugadas.append(pjugada + ";");
+    }
+}
